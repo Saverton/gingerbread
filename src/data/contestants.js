29f3e4259595a0306322fee8ee33a97c9827d91f
Scott Meadows <Scottmeadows04@gmail.com>
2022-12-15 17:07:21 -0500

    add basic data

---
+++ src/data/contestants.js
@@ -0,0 +1,43 @@
+const CONTESTANTS = [
+  {
+    id: 1,
+    name: 'Scott M.',
+    imageUrl: '../images/contestants/01.jpeg',
+    houseId: 1,
+    quote: 'Gingerbread house making is an art accessible to anyone, that is, assuming they have real gingerbread.',
+    placed: '1st'
+  },
+  {
+    id: 2,
+    name: 'Landen F.',
+    imageUrl: '../images/contestants/02.jpeg',
+    houseId: 2,
+    quote: '😂😂😂',
+    placed: '4th'
+  },
+  {
+    id: 3,
+    name: 'Ben K.',
+    imageUrl: '../images/contestants/03.jpeg',
+    houseId: 3,
+    quote: 'PLACEHOLDER',
+    placed: '2nd'
+  },
+  {
+    id: 4,
+    name: 'Sam Y.',
+    imageUrl: '../images/contestants/04.jpeg',
+    houseId: 4,
+    quote: 'Creativity and Google Images are not synonymous.',
+    placed: '2nd'
+  },
+  {
+    id: 5,
+    name: '"Terrible Trio"',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC4KmBaehR0ZHUcqSm3cLxL4OWJO1Pbef-Ag&usqp=CAU',
+    imageCaption: 'Artist Rendition',
+    houseId: 5,
+    quote: 'https://www.youtube.com/watch?v=z8x2fAE0h3w',
+    placed: '5th'
+  }
+]
---
+++ src/data/gingerbreadHouses.js
@@ -0,0 +1,62 @@
+const HOUSES = [
+  {
+    id: 1,
+    designerId: 1,
+    imageUrl: '../images/houses/01.jpeg',
+    points: {
+      sweetness: 5,
+      structuralEngineering: 4,
+      landscaping: 10,
+      aesthetics: 10,
+      imagniation: 9,
+    }
+  },
+  {
+    id: 2,
+    designerId: 2,
+    imageUrl: '../images/houses/02.jpeg',
+    points: {
+      sweetness: 10,
+      structuralEngineering: 4,
+      landscaping: 3,
+      aesthetics: 5,
+      imagniation: 5,
+    }
+  },
+  {
+    id: 3,
+    designerId: 3,
+    imageUrl: '../images/houses/03.jpeg',
+    points: {
+      sweetness: 8,
+      structuralEngineering: 2,
+      landscaping: 7,
+      aesthetics: 6,
+      imagniation: 6,
+    }
+  },
+  {
+    id: 4,
+    designerId: 4,
+    imageUrl: '../images/houses/04.jpeg',
+    points: {
+      sweetness: 7,
+      structuralEngineering: 5,
+      landscaping: 3,
+      aesthetics: 7,
+      imagniation: 7,
+    }
+  },
+  {
+    id: 5,
+    designerId: 5,
+    imageUrl: '../images/houses/05.jpeg',
+    points: {
+      sweetness: 2,
+      structuralEngineering: 10,
+      landscaping: 0,
+      aesthetics: 1,
+      imagniation: 10,
+    }
+  }
+]
