3b69cab4d527bc42e8e0a578388e9c2c8c739940
Scott Meadows <Scottmeadows04@gmail.com>
2022-12-15 17:46:42 -0500

    get react router working again

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,22 @@
 import React from 'react';
-import { createBrowserRouter, Route, RouterProvider } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Houses from './Houses';
 import Home from './Home';
 import Contestants from './Contestants';
 import Header from './Header';
 import Footer from './Footer';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: (
-      <Home />
-    )
-  },
-  {
-    path: '/contestants/:id',
-    element: (
-      <Contestants />
-    )
-  },
-  {
-    path: '/houses/:id',
-    element: (
-      <Houses />
-    )
-  }
-]);
 
 function App() {
   return (
     <div>
-      <h1>Hello, World!</h1>
-      <RouterProvider router={router} />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/contestants/:id" element={<Contestants />}></Route>
+        <Route path="/houses/:id" element={<Houses />}></Route>
+      </Routes>
+      <Footer />
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
-  return <h1>Header</h1>
+  return (
+    <header>
+      <h1>The Great 2022 Gingerbread Tournament</h1>
+      <nav>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/contestants/1'>Contestants</NavLink>
+        <NavLink to='/houses/1'>Gingerbread Houses</NavLink>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
