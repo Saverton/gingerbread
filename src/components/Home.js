f06e313413a6af5f21f98ff723f6803e39e93bc4
Scott Meadows <Scottmeadows04@gmail.com>
2022-12-21 11:39:01 -0500

    fix header and footer mobile

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import CONTESTANTS from '../data/contestants';
 import HOUSES from '../data/gingerbreadHouses';
 import HouseCard from './HouseCard';
 
@@ -13,7 +12,7 @@ function Home() {
         <p>
           This webpage is intended to recognize the incredible accomplishments of the various
           contestants of this gingerbread house making contest. A special thank you goes out 
-          to our contest Judge, Jeff T., and to the homemade gingerbread maker, Ginger M.
+          to our impossibly unbiased contest Judge, Jeff T., and to the homemade gingerbread maker, Ginger M.
         </p>
       </article>
       <h2>Meet the winner...</h2>
